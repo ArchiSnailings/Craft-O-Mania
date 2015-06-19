c560c552daa5981472ad9d3a72ef0b8f8c5ff080
Chrezz <chresser_900@hotmail.com>
2015-06-19 14:30:04 +0200

    Basic tile class

---
+++ Craftings/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Craftings
+{
+    class Tile {
+
+        int _posX;
+        int _posY;
+        int _posZ;
+
+        string _name = "default";
+
+        Tile(int posX, int posY, int posZ) {
+            _posX = posX;
+            _posY = posY;
+            _posZ = posZ;
+        }
+
+        
+    }
+}
