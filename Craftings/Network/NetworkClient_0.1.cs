9dece1c9cb3441f78e5e4fdb1623fa6252eba117
Arne Rasmussen <Gedesneglen@gmail.com>
2015-06-20 14:07:51 +0200

    Added Inetworkclient and networkclient_0.1

---
+++ Craftings/Network/NetworkClient_0.1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Craftings.Network
+{
+    class NetworkClient_0 : INetworkClient
+    {
+        
+    }
+}
